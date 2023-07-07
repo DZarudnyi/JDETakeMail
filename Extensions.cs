f2ef51dfce97a3d66636e2fce955768de40a32af
DZarudnyi <dzarudnyi.work@gmail.com>
2023-07-07 12:04:30 +0300

    changed framework version

---
+++ Extensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace JDETakeMail
+{
+    static class Extensions
+    {
+        public static string Right(this string value, int length)
+        {
+            if (value.Length < length)
+                return value;
+            return value.Substring(value.Length - length);
+        }
+
+        public static bool IsNumber(this string value)
+        {
+            //return !Regex.IsMatch(value, "[^0-9]");
+            return int.TryParse(value, out _);
+        }
+    }
+}
